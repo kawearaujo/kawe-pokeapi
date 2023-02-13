8bac47a8430bbd6f35b61d8ebd357227da54deb5
Kawê <kawe.apr@gmail.com>
2023-02-12 23:55:23 -0300

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     
+    <Header /> 
 
   </div>
 </template>
 
 <script>
+import './styles/globals.css';
 
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-
+    Header
   }
 }
 </script>
